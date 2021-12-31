99ee5f6c09d510fe754f7687acd9fbe2c0068076
bogdanjianu150 <82314714+bogdanjianu150@users.noreply.github.com>
2021-12-31 02:56:49 +0200

    Add files via upload

---
+++ p1/db.php
@@ -0,0 +1,22 @@
+<?php
+
+class db
+{
+    protected $servername = 'localhost';
+    protected $username = 'root';
+    protected $password = 'Semaffor71!';
+    protected $dbname = 'biblioteca';
+    protected $conectare;
+    public function SetConectare()
+    {
+
+        $this->conectare = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+        $this->conectare->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+
+
+    public function getConectare(){
+        return $this->conectare;
+}
+}
